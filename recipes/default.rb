d6ea954eab97a9ea2ff5331d414f474139ee5faf
Eric Herot <eric.github@herot.com>
2013-07-10 16:09:44 -0400

    Optionalize query logging, ipv6 support, and dnssec.  Bump version to v1.0.5

--- recipes/default.rb
+++ recipes/default.rb
@@ -48,6 +48,21 @@ node['ec2dnsserver']['zones'].each do |zone|
   end
 end
 
+init_config_file = value_for_platform(
+  ['ubuntu','debian'] => {"default" => "/etc/default/bind9"},
+  ["centos", "redhat", "suse", "fedora", "amazon", "amazonaws"] => {
+    "default" => "/etc/sysconfig/named"
+  }
+)
+
+file init_config_file do
+  if node['ec2dnsserver']['enable-ipv6']
+    content "RESOLVCONF=no\nOPTIONS=-u bind\n"
+  else
+    content "RESOLVCONF=no\nOPTIONS=\"-u bind -4\n\""
+  end
+end
+
 template "/etc/logrotate.d/named" do
   source "logrotate.conf.erb"
   owner "root"
