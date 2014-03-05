90b8a6c7bf0f2645b0b413cb341fc8d9ac42b5a0
Eric Herot <eric.github@herot.com>
2014-03-04 19:10:36 -0500

    Add stub zones support; Also fix some linting/rubocop errors

--- providers/zone.rb
+++ providers/zone.rb
@@ -2,38 +2,53 @@ use_inline_resources
 
 action :create do
   dns_server = Chef::Recipe::Ec2DnsServer.new(node, node.chef_environment)
-  hosts = dns_server.get_names_with_ips({
-    'vpc-id' => new_resource.vpc,
-    'avoid_subnets' => new_resource.avoid_subnets,
-    'static_records' => new_resource.static_records
-  })
 
   # In the template, the source host and the apex are both supposed
   # to end with dots in SOME places in the template. We're not going
   # to assume the recipe writer knows how we're going to deal with
   # this, so we'll just always remove the dot and add it later if we
   # need it.
+  apex = new_resource.apex.sub(/\.$/, '')
+  source_host = new_resource.source_host.sub(/\.$/, '')
 
-  source_host = new_resource.source_host.sub(/\.$/,'')
-  apex = new_resource.apex.sub(/\.$/,'')
+  hosts = dns_server.get_names_with_ips(
+    apex,
+    new_resource.stub,
+    'vpc-id' => new_resource.vpc,
+    'avoid_subnets' => new_resource.avoid_subnets,
+    'static_records' => new_resource.static_records
+  )
+
+  # We need a suffix but stub zones don't inherently have them
+  if new_resource.suffix.empty? && new_resource.soa_zone.empty?
+    fail "#{apex}: Either suffix or soa_zone must be specified"
+  elsif !new_resource.suffix.empty?
+    suffix = new_resource.suffix
+    is_primary = true
+  else
+    suffix = new_resource.soa_zone
+    is_primary = false
+  end
 
   template new_resource.path do
     source "zone.erb"
     mode 00644
     group "bind"
     variables(
-      :hosts => hosts,
-      :apex => apex,
-      :source_host => source_host,
-      :ptr => new_resource.ptr,
-      :suffix => new_resource.suffix,
-      :serial_number => Time.now.to_i,
-      :default_ttl => new_resource.default_ttl,
-      :contact_email => new_resource.contact_email.sub("@","."),
-      :refresh_time => new_resource.refresh_time,
-      :retry_time => new_resource.retry_time,
-      :expire_time => new_resource.expire_time,
-      :min_ttl => new_resource.min_ttl
+      hosts: hosts,
+      apex: apex,
+      is_primary: is_primary,
+      stub: new_resource.stub,
+      source_host: source_host,
+      ptr: new_resource.ptr,
+      suffix: suffix,
+      serial_number: Time.now.to_i,
+      default_ttl: new_resource.default_ttl,
+      contact_email: new_resource.contact_email.sub('@', '.'),
+      refresh_time: new_resource.refresh_time,
+      retry_time: new_resource.retry_time,
+      expire_time: new_resource.expire_time,
+      min_ttl: new_resource.min_ttl
     )
     action :nothing
   end
@@ -63,18 +78,20 @@ action :create do
     source "zone.erb"
     mode 00644
     variables(
-      :hosts => hosts,
-      :apex => apex,
-      :source_host => source_host,
-      :ptr => new_resource.ptr,
-      :suffix => new_resource.suffix,
-      :serial_number => "",
-      :default_ttl => new_resource.default_ttl,
-      :contact_email => new_resource.contact_email.sub("@","."),
-      :refresh_time => new_resource.refresh_time,
-      :retry_time => new_resource.retry_time,
-      :expire_time => new_resource.expire_time,
-      :min_ttl => new_resource.min_ttl
+      hosts: hosts,
+      apex: apex,
+      is_primary: is_primary,
+      stub: new_resource.stub,
+      source_host: source_host,
+      ptr: new_resource.ptr,
+      suffix: suffix,
+      serial_number: '',
+      default_ttl: new_resource.default_ttl,
+      contact_email: new_resource.contact_email.sub('@', '.'),
+      refresh_time: new_resource.refresh_time,
+      retry_time: new_resource.retry_time,
+      expire_time: new_resource.expire_time,
+      min_ttl: new_resource.min_ttl
     )
     notifies :create, "template[#{new_resource.path}]"
   end
