ab1da025b14c1339d5aac23a71d1e4d3712002d8
Jeff Byrnes <thejeffbyrnes@gmail.com>
2016-03-21 16:00:21 -0400

    Refactor LWRP into a Chef 12.5 custom resource

--- resources/zone.rb
+++ resources/zone.rb
@@ -20,22 +20,132 @@
 actions :create, :delete
 default_action :create
 
-attribute :apex,                  kind_of: String,            name_attribute: true
-attribute :path,                  kind_of: String,            default: nil
-attribute :suffix,                kind_of: String,            default: nil
-attribute :source_host,           kind_of: String,            default: node.name
-attribute :ptr,                   equal_to: [true, false],    default: false
-attribute :default_ttl,           kind_of: Fixnum,            default: 300
-attribute :contact_email,         kind_of: String,            required: true
-attribute :refresh_time,          kind_of: [String, Fixnum],  default: '3600'
-attribute :retry_time,            kind_of: [String, Fixnum],  default: '600'
-attribute :expire_time,           kind_of: [String, Fixnum],  default: '86400'
-attribute :nxdomain_ttl,          kind_of: [String, Fixnum],  default: '300'
-attribute :vpcs,                  kind_of: Array,             default: []
-attribute :static_records,        kind_of: Hash,              default: {}
-attribute :avoid_subnets,         kind_of: Array,             default: []
-attribute :stub,                  equal_to: [true, false],    default: false
-attribute :ns_zone,               kind_of: String,            default: nil
-attribute :aws_access_key_id,     kind_of: String
-attribute :aws_secret_access_key, kind_of: String
-attribute :mocking,               equal_to: [true, false],    default: false
+property :apex,                  String,           name_attribute: true
+property :path,                  [String, nil],    default: nil
+property :suffix,                [String, nil],    default: nil
+property :source_host,           String,           default: node.name
+property :ptr,                                     default: false
+property :default_ttl,           Fixnum,           default: 300
+property :contact_email,         String,                                  required: true
+property :refresh_time,          [String, Fixnum], default: '3600'
+property :retry_time,            [String, Fixnum], default: '600'
+property :expire_time,           [String, Fixnum], default: '86400'
+property :nxdomain_ttl,          [String, Fixnum], default: '300'
+property :vpcs,                  Array,            default: []
+property :static_records,        Hash,             default: {}
+property :avoid_subnets,         Array,            default: []
+property :stub,                                    default: false
+property :ns_zone,               [String, nil],    default: nil
+property :aws_access_key_id,     String
+property :aws_secret_access_key, String
+property :mocking,                                 default: false
+
+action :create do
+  # In the template, the source host and the apex are both supposed
+  # to end with dots in SOME places in the template. We're not going
+  # to assume the recipe writer knows how we're going to deal with
+  # this, so we'll just always remove the dot and add it later if we
+  # need it.
+  apex = new_resource.apex.sub(/\.$/, '')
+  source_host = new_resource.source_host.sub(/\.$/, '')
+  path = new_resource.path || "#{node['ec2dnsserver']['zones_dir']}/db.#{apex}"
+
+  zone_options = {
+    'avoid_subnets' => new_resource.avoid_subnets,
+    'static_records' => new_resource.static_records
+  }
+
+  if new_resource.aws_access_key_id
+    zone_options[:conn_opts] = {
+      aws_access_key_id: new_resource.aws_access_key_id,
+      aws_secret_access_key: new_resource.aws_secret_access_key
+    }
+  end
+
+  dns_server =
+    Chef::Recipe::Ec2DnsServer.new(node.chef_environment, apex, zone_options)
+  dns_server.mock! if new_resource.mocking || node['ec2dnsserver']['mocking']
+
+  hosts = dns_server.hosts(new_resource.stub, new_resource.vpcs)
+
+  Chef::Log.debug("Zone: #{apex}")
+  Chef::Log.debug("Hosts: #{hosts.inspect}")
+
+  primary = new_resource.ns_zone && new_resource.ns_zone == apex
+
+  Chef::Log.info("Zone #{apex} #{primary ? 'is' : 'is NOT'} a primary zone.")
+
+  template path do
+    source 'zone.erb'
+    mode 00644
+    group 'bind'
+    variables(
+      hosts: hosts,
+      apex: apex,
+      is_primary: primary,
+      stub: new_resource.stub,
+      source_host: source_host,
+      ptr: new_resource.ptr,
+      suffix: new_resource.suffix.nil? ? apex : new_resource.suffix,
+      serial_number: Time.now.to_i,
+      default_ttl: new_resource.default_ttl,
+      contact_email: new_resource.contact_email.sub('@', '.'),
+      refresh_time: new_resource.refresh_time,
+      retry_time: new_resource.retry_time,
+      expire_time: new_resource.expire_time,
+      nxdomain_ttl: new_resource.nxdomain_ttl,
+      ns_zone: new_resource.ns_zone.nil? ? apex : new_resource.ns_zone
+    )
+    action :nothing
+  end
+
+  directory "#{Chef::Config[:file_cache_path]}/ec2dnsserver/zones_without_serials" do
+    recursive true
+  end
+
+  # This next bit seems like a kludge (and it is) but the purpose of it
+  # is to ONLY update the zone file when there is an actual zone
+  # change (rather than every time chef-client runs), while still using
+  # Time.now.to_i to generate the zone serial number.  Since we're
+  # treating the serial number as a template variable, chef would normally
+  # try to update this template (and thus the serial number) every time it
+  # runs.  Instead we have a second "dummy" file tied to the same exact
+  # same variables (minus the serial number) that notifies the "real"
+  # template whenever it gets updated, and otherwise the real template
+  # doesn't update (so the serial number doesn't change).
+  # Nice, huh? ;-)
+  #   -- devops@evertrue.com
+
+  directory ::File.dirname("#{Chef::Config[:file_cache_path]}/ec2dnsserver/zones_without_serials" +
+      path) do
+    recursive true
+  end
+
+  template "#{Chef::Config[:file_cache_path]}/ec2dnsserver/zones_without_serials" + path do
+    source 'zone.erb'
+    variables(
+      hosts: hosts,
+      apex: apex,
+      is_primary: primary,
+      stub: new_resource.stub,
+      source_host: source_host,
+      ptr: new_resource.ptr,
+      suffix: new_resource.suffix.nil? ? apex : new_resource.suffix,
+      serial_number: '',
+      default_ttl: new_resource.default_ttl,
+      contact_email: new_resource.contact_email.sub('@', '.'),
+      refresh_time: new_resource.refresh_time,
+      retry_time: new_resource.retry_time,
+      expire_time: new_resource.expire_time,
+      nxdomain_ttl: new_resource.nxdomain_ttl,
+      ns_zone: new_resource.ns_zone.nil? ? apex : new_resource.ns_zone
+    )
+    notifies :create, "template[#{path}]"
+  end
+end
+
+action :delete do
+  file new_resource.path do
+    action :delete
+  end
+end
