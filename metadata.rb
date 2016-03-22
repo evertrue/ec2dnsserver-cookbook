73d6725bd7ce083424579a66391ba2362bdfffb3
Jeff Byrnes <thejeffbyrnes@gmail.com>
2016-03-22 11:45:39 -0400

    Add source and issues URLs for Supermarket

--- metadata.rb
+++ metadata.rb
@@ -7,5 +7,8 @@ description      'Installs/Configures ec2dnsserver'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '2.2.1'
 
+issues_url 'https://github.com/evertrue/ec2dnsserver-cookbook/issues' if respond_to? :issues_url
+source_url 'https://github.com/evertrue/ec2dnsserver-cookbook/' if respond_to? :source_url
+
 depends 'build-essential'
 depends 'et_fog', '~> 1.0'
