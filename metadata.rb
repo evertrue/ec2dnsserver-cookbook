5a2b07795cde953f276419f0ec6e8038685051c9
Jeff Byrnes <thejeffbyrnes@gmail.com>
2016-03-22 11:14:49 -0400

    Update to use Apache v2 license

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@
 name             'ec2dnsserver'
 maintainer       'EverTrue, Inc.'
 maintainer_email 'devops@evertrue.com'
-license          'All rights reserved'
+license          'Apache 2.0'
 description      'Installs/Configures ec2dnsserver'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '2.2.0'
