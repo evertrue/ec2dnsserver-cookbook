3aaed8fee6f7c2527b4863aa88e2243e3cf7e4cb
Jeff Byrnes <thejeffbyrnes@gmail.com>
2016-03-22 11:47:12 -0400

    Bump to v2.2.2

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ maintainer_email 'devops@evertrue.com'
 license          'Apache 2.0'
 description      'Installs/Configures ec2dnsserver'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '2.2.1'
+version          '2.2.2'
 
 issues_url 'https://github.com/evertrue/ec2dnsserver-cookbook/issues' if respond_to? :issues_url
 source_url 'https://github.com/evertrue/ec2dnsserver-cookbook/' if respond_to? :source_url
